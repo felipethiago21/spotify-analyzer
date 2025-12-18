378f569261321ae82272a6925578374bd1988b1f
Felipe Thiago C. C. Oliveira <98181566+felipethiago21@users.noreply.github.com>
2025-12-17 23:41:39 -0300

    Update spotify_client.py

--- spotify_client.py
+++ spotify_client.py
@@ -1,17 +1,19 @@
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def get_spotify_client():
-    return spotipy.Spotify(
-        auth_manager=SpotifyOAuth(
-            client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-            client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
-            redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
-            scope="user-top-read user-read-recently-played",
-            cache_path=".spotify_cache"
-        )
+    scope = "user-top-read"
+
+    auth_manager = SpotifyOAuth(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+        scope=scope,
+        open_browser=True,          # 🔴 ESSENCIAL NO STREAMLIT CLOUD
+        cache_path=".spotify_cache" # cache local
     )
+
+    return spotipy.Spotify(auth_manager=auth_manager)
+
